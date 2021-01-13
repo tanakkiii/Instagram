a561c966ceceb7ed0d428ae42c26c762610788eb
tanakkiii <tanakkiii@gmail.com>
2021-01-14 01:33:57 +0900

    カリキュラムで指定された機能まで実装

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 田中翔悟 on 2021/01/13.
+//  Copyright © 2021 shogo.tanaka. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
